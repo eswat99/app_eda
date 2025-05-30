99f17cb6e9fe24b8c8ed7c4ad53870bda94f6e56
eswat99 <eswat99@nate.com>
2025-05-30 11:11:12 +0900

    update?

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# ---------------------
+# ----------------------
 # Firebase 설정
 # ---------------------
 firebase_config = {
